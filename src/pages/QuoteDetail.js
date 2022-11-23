e2fabba6faf3e951a712aa9359fe7e461cf23bd0
lalithcodelover <lalith.vlr@gmail.com>
2022-11-23 23:29:16 +0530

    change first added

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
-
-const DUMMY_QUOTES = [
-  { id: "q1", author: "John", text: "Learning React is Fun!!" },
-  { id: "q2", author: "JohnCena", text: "Learning Coding is Awesome!!" },
-];
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+// const DUMMY_QUOTES = [
+//   { id: "q1", author: "John", text: "Learning React is Fun!!" },
+//   { id: "q2", author: "JohnCena", text: "Learning Coding is Awesome!!" },
+// ];
 
 const QuoteDetail = () => {
   const params = useParams();
-const match = useRouteMatch()
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  const match = useRouteMatch();
+  const { quoteId } = params;
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getSingleQuote, true);
 
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
   if (!quote) {
     return <p>No quote found!!</p>;
   }
